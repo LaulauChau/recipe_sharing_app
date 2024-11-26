79e16b4dbbef7a0d1a303c56f9fb0334485bcc62
Laurent Chau <mplaurent.chau@gmail.com>
2024-11-26 09:24:41 +0100

    feat(angular): add firebase

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,4 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
@@ -7,5 +10,17 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: "recipe-sharing-app-6ec13",
+        appId: "1:688436745851:web:2377ed1e0d4251eb40f4de",
+        storageBucket: "recipe-sharing-app-6ec13.firebasestorage.app",
+        apiKey: "AIzaSyAohTofetaMb4fk0YIVVTXP7AxQT0LKDao",
+        authDomain: "recipe-sharing-app-6ec13.firebaseapp.com",
+        messagingSenderId: "688436745851",
+      }),
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
