68ae23d4142d608b5c961f931ec5a5695136fca5
Laurent Chau <mplaurent.chau@gmail.com>
2024-11-30 20:48:46 +0100

    add logging to see if env exists

--- apps/web/generate-vercel-env.js
+++ apps/web/generate-vercel-env.js
@@ -1,5 +1,12 @@
 const fs = require("node:fs");
 
+console.log("project id", process.env.FIREBASE_PROJECT_ID);
+console.log("app id", process.env.FIREBASE_APP_ID);
+console.log("storage bucket", process.env.FIREBASE_STORAGE_BUCKET);
+console.log("api key", process.env.FIREBASE_API_KEY);
+console.log("auth domain", process.env.FIREBASE_AUTH_DOMAIN);
+console.log("messaging sender id", process.env.FIREBASE_MESSAGING_SENDER_ID);
+
 const envContent = `export const environment = {
   firebase: {
     projectId: '${process.env.FIREBASE_PROJECT_ID}',
